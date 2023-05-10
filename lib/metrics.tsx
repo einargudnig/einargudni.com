3888f55b68e8803adf473903856c329c4acc0510
Einar <einargudnig@gmail.com>
2023-05-10 21:08:28 +0000

    db connection

---
+++ lib/metrics.tsx
@@ -0,0 +1,31 @@
+import 'server-only';
+
+import { Octokit } from '@octokit/rest';
+import { queryBuilder } from 'lib/planetscale';
+import { cache } from 'react';
+
+export const getBlogViews = cache(async () => {
+  if (!process.env.TWITTER_API_TOKEN) {
+    return 0;
+  }
+
+  const data = await queryBuilder
+    .selectFrom('views')
+    .select(['count'])
+    .execute();
+
+  return data.reduce((acc, curr) => acc + Number(curr.count), 0);
+});
+
+export const getStarCount = cache(async () => {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+  const req = await octokit.request('GET /repos/{owner}/{repo}', {
+    owner: 'leerob',
+    repo: 'leerob.io',
+  });
+
+  return req.data.stargazers_count;
+});
