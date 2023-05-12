8bf29b178fdc31df857870599b983af698b39647
Einar <einargudnig@gmail.com>
2023-05-12 23:30:06 +0000

    more ui, more content 🍷

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import './globals.css'
+import clsx from 'clsx';
 import { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local';
 import Sidebar from '../components/sidebar'
+import { Analytics } from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ['latin'] })
+const kaisei = localFont({
+  src: '../public/fonts/kaisei-tokumin-latin-700-normal.woff2',
+  weight: '700',
+  variable: '--font-kaisei',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Einar Gudni',
@@ -29,11 +36,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en" className={clsx(
+        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        kaisei.variable
+      )}>
       <body className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
         <Sidebar />
         <main className='flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0'>
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
