817891926d9a50c95b34939bae79d961c90ed1ca
Einar <einargudnig@gmail.com>
2023-05-22 08:38:20 +0000

    need to test the location thingy

---
+++ pages/api/location.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type LocationData = {
+  city: string;
+  region: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<LocationData>
+) {
+  if (req.method === 'POST') {
+    const { city, region } = req.body;
+    // You can now use the latitude and longitude in your Next.js app or perform any necessary processing
+    const locationData: LocationData = { city, region };
+
+    res.status(200).json(locationData);
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
