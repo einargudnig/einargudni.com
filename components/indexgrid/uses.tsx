e559952e61ff07a9412f19d1632c128a43d4e0b0
Einar <einargudnig@gmail.com>
2023-06-05 21:45:29 +0000

    updated spotify component

--- components/indexgrid/uses.tsx
+++ components/indexgrid/uses.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
+import { UsesIcon } from '../icons';
 
 export default function UsesButton() {
 
   return (
     <div className='h-48 border border-gray-200 rounded-md'>
-    <Link href={`/uses`} className="flex items-center">
-      uses
+      
+    <Link href={`/uses`} className="flex items-center justify-center">
+      <UsesIcon />{' '}uses
     </Link>
     </div>
   );
