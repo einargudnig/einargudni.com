4780fc3ff8a075a77ffba22a3bf7fe210c502dc0
Einar <einargudnig@gmail.com>
2023-06-20 23:05:54 +0000

    strapped in 🚀👩‍🚀

--- components/indexgrid/hobbies.tsx
+++ components/indexgrid/hobbies.tsx
@@ -1,10 +1,24 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { HobbiesIcon } from '../icons';
 
 export default function HobbiesButton() {
 	return (
-		<div className="h-48 border border-gray-200 rounded-md">
-			<Link href={'/hobbies'} className="flex items-center">
-				hobbies
+		<div className="h-90 p-4 border border-gray-200 rounded-md shadow-md max-w-md mx-auto">
+			<Link href={'/uses'} className="flex items-center justify-center">
+				<div>
+					<Image
+						src="/images/hobbies.png"
+						alt="hobbies"
+						className="rounded-lg grayscale"
+						width={350}
+						height={100}
+					/>
+					<div className="flex flex-row mx-auto mt-6">
+						<HobbiesIcon />
+						<p className="ml-2">hobbies</p>
+					</div>
+				</div>
 			</Link>
 		</div>
 	);
--- components/indexgrid/now.tsx
+++ components/indexgrid/now.tsx
@@ -1,10 +1,24 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { GlobeIcon } from '../icons';
 
 export default function NowButton() {
 	return (
-		<div className="h-48 border border-gray-200 rounded-md">
-			<Link href={'/now'} className="flex items-center">
-				Now
+		<div className="h-90 p-4 border border-gray-200 rounded-md shadow-md max-w-md mx-auto">
+			<Link href={'/uses'} className="flex items-center justify-center">
+				<div>
+					<Image
+						src="/images/now.png"
+						alt="now"
+						className="rounded-lg grayscale"
+						width={350}
+						height={100}
+					/>
+					<div className="flex flex-row mx-auto mt-6">
+						<GlobeIcon />
+						<p className="ml-2">now</p>
+					</div>
+				</div>
 			</Link>
 		</div>
 	);
