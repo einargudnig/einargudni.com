1aadcba4494ed40e49dca890a3d98d3f0d621aa6
Einar <einargudnig@gmail.com>
2024-01-10 08:52:39 +0100

    addig geist + fixing layout shift

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,13 @@
 import './globals.css';
-import clsx from 'clsx';
 import { Metadata } from 'next';
-import localFont from 'next/font/local';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Analytics } from '@vercel/analytics/react';
 import { Navbar } from '@/components/navbar';
 import { CommandMenu } from '@/components/command-menu';
 import { RESUME_DATA } from '@/data/resume-data';
-// import { GeistSans } from 'geist/font/sans';
-// import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
 
-const kaisei = localFont({
-	src: '../public/fonts/kaisei-tokumin-latin-700-normal.woff2',
-	weight: '700',
-	variable: '--font-kaisei',
-	display: 'swap'
-});
-
-// I should not need to this. There is an npm pacakage for this I think?
-// const geist = localFont({
+// const kaisei = localFont({
 // 	src: '../public/fonts/kaisei-tokumin-latin-700-normal.woff2',
 // 	weight: '700',
 // 	variable: '--font-kaisei',
@@ -45,11 +34,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		// <html lang="en" className={GeistSans.className}>
-		<html lang="en" className={clsx(kaisei.variable)}>
+		<html lang="en" className={GeistSans.className}>
+			{/* <html lang="en" className={clsx(kaisei.variable)}> */}
 			<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 				<body className="antialiased mb-10 mx-4 mt-8 lg:mx-auto">
-					<main className="container relative mx-auto scroll-my-12 overflow-auto print:p-12">
+					<main className="container relative mx-auto overflow-auto print:p-12">
 						<Navbar />
 						{children}
 						<CommandMenu
