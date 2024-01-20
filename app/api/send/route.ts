a5631e35f085da42556f267eb558564192109766
Einar <einargudnig@gmail.com>
2024-01-20 01:25:55 +0100

    setting up resend

---
+++ app/api/send/route.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { EmailTemplate } from '@/components/email-template';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+	try {
+		const data = await resend.emails.send({
+			from: 'Acme <onboarding@resend.dev>',
+			to: ['delivered@resend.dev'],
+			subject: 'Hello world',
+			react: EmailTemplate({ firstName: 'John' })
+		});
+
+		return Response.json(data);
+	} catch (error) {
+		return Response.json({ error });
+	}
+}
