e0dcaa6190bb51e5c592ccabc46c0b8817549e9d
Einar <einargudnig@gmail.com>
2024-04-01 23:30:04 +0000

    react-email work

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,4 +1,5 @@
-import { EmailTemplate } from '@/components/email-template';
+// import { EmailTemplate } from '@/components/email-template';
+import { NotionMagicLinkEmail } from '@/react-email-starter/emails/notion-magic-link';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -6,10 +7,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
 	try {
 		const data = await resend.emails.send({
-			from: 'Acme <onboarding@resend.dev>',
+			from: 'Einar Gudni <triple-three@einargudni.com>',
 			to: ['delivered@resend.dev'],
-			subject: 'Hello world',
-			react: EmailTemplate({ firstName: 'John' })
+			subject: 'Triple-three #01',
+			react: NotionMagicLinkEmail
 		});
 
 		return Response.json(data);
