2c9233b620a27e30e5fcf5b485908cbf1f9488bf
Einar <einargudnig@gmail.com>
2024-10-18 08:24:50 +0000

    more dissecting

--- app/dissect/page.tsx
+++ app/dissect/page.tsx
@@ -2,9 +2,15 @@ import { Separator } from '@radix-ui/react-separator';
 import { Link } from 'next-view-transitions';
 
 export default function Page() {
+	const dissectData = [
+		{ text: 'Marchon training app', url: 'marchon', icon: '🔍', status: 'in progress' },
+		{ text: 'Raycast', url: 'raycast', icon: '🔍', status: 'in progress' },
+		{ text: 'Things 3', url: 'things', icon: '🔍', status: 'in progress' }
+	];
+
 	return (
 		<section className="mx-auto w-full max-w-2xl space-y-8 print:space-y-6">
-			<h2 className="text-xl font-bold">App dissect</h2>
+			<h2 className="text-xl font-bold">Software dissection</h2>
 			<p className="text-muted-foreground mt-4">
 				Here I will dissect some of the software I use on daily bases!
 			</p>
