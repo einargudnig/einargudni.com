5460238c5a6358e956200aaafd42ed82684ab0e4
Einar <einargudnig@gmail.com>
2024-10-20 22:55:11 +0000

    more dissect

--- app/dissect/page.tsx
+++ app/dissect/page.tsx
@@ -1,26 +1,50 @@
 import { Separator } from '@radix-ui/react-separator';
 import { Link } from 'next-view-transitions';
+import raycastIcon from '../../public/images/dissect/raycast/raycast.png';
+import marchonIcon from '../../public/images/dissect/marchon/marchon.png';
+import thingsIcon from '../../public/images/dissect/things/things.png';
+import Image from 'next/image';
 
 export default function Page() {
 	const dissectData = [
-		{ text: 'Marchon training app', url: 'marchon', icon: '🔍', status: 'in progress' },
-		{ text: 'Raycast', url: 'raycast', icon: '🔍', status: 'in progress' },
-		{ text: 'Things 3', url: 'things', icon: '🔍', status: 'in progress' }
+		{ text: 'Marchon training app', url: 'marchon', icon: marchonIcon, status: 'October 2024' },
+		{ text: 'Raycast', url: 'raycast', icon: raycastIcon, status: 'in progress' },
+		{ text: 'Things 3', url: 'things', icon: thingsIcon, status: 'in progress' }
 	];
 
 	return (
 		<section className="mx-auto w-full max-w-2xl space-y-8 print:space-y-6">
 			<h2 className="text-xl font-bold">Software dissection</h2>
 			<p className="text-muted-foreground mt-4">
-				Here I will dissect some of the software I use on daily bases!
+				Here I will dissect some of the software I use on daily bases! Some software just
+				has something extra. This can be a feature, a design choice or a workflow that just
+				works for me.
+			</p>
+			<p className="text-muted-foreground mt-4 text-xs">
+				Note! This is just my personal preference 🤓
 			</p>
 			<Separator className="bg-muted-foreground w-24" />
-			<div className="flex flex-col items-start w-full border-1 border-red-300">
-				{/* <Link href="../dissect/marchon">Marchon</Link> */}
+			<div className="flex flex-col items-start">
 				{dissectData.map((item) => (
-					<div key={item.text} className="flex items-center justify-between">
-						<Link href={`../dissect/${item.url}`}>{item.text}</Link>
-						<span className="text-muted-foreground">{item.status}</span>
+					<div key={item.text} className="flex items-center justify-between w-full p-1">
+						<Link href={`../dissect/${item.url}`} className="w-full">
+							<div className="flex items-center">
+								<Image
+									src={item.icon}
+									className="rounded-md"
+									alt="software logo"
+									height={40}
+									width={40}
+								/>
+								<div className="flex items-center justify-between ml-2 w-full">
+									{item.text}
+
+									<span className="text-muted-foreground font-mono">
+										{item.status}
+									</span>
+								</div>
+							</div>
+						</Link>
 					</div>
 				))}
 			</div>
